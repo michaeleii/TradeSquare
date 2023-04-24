c0544363edb16e7bbcb8d2feb5c4a46a8acf3b6e
Xiao Zhang <xxxiao971006@gmail.com>
2023-04-24 16:44:58 -0700

    initial

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("pages/index"));
 
 import squaresRouter from "./routes/squares";
 
--- routes/squares.ts
+++ routes/squares.ts
@@ -5,17 +5,22 @@ const squares = express.Router();
 squares.get("/all", (req, res) => {
 	res.send("All the squares");
 });
-squares.get("/:id", (req, res) => {
-	res.send("Square for id: " + req.params.id);
-});
+
+// squares.get("/:id", (req, res) => {
+// 	res.send("Square for id: " + req.params.id);
+// });
 
 squares.get("/squarecomponent", (req, res) => {});
 
-squares.get("/squaretitlecomponent", (req, res) => {});
+squares.get("/squaretitlecomponent", (req, res) => {
+	res.render('components/Square_Title_and_Description')
+});
 
 squares.get("/joinsquarebtncomponent", (req, res) => {});
 
-squares.get("/postcomponent", (req, res) => {});
+squares.get("/postcomponent", (req, res) => {
+	res.render('components/postCard')
+});
 
 squares.get("/subnavcomponent", (req, res) => {});
 
