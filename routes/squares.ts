d4926072e257a7e577154114ccefb59237872f34
jerry <jerryfan71@gmail.com>
2023-04-24 16:44:14 -0700

    finish sub-navigation

--- routes/squares.ts
+++ routes/squares.ts
@@ -5,9 +5,11 @@ const squares = express.Router();
 squares.get("/all", (req, res) => {
 	res.send("All the squares");
 });
-squares.get("/:id", (req, res) => {
-	res.send("Square for id: " + req.params.id);
-});
+
+
+// squares.get("/:id", (req, res) => {
+// 	res.send("Square for id: " + req.params.id);
+// });
 
 squares.get("/squarecomponent", (req, res) => {});
 
@@ -17,6 +19,8 @@ squares.get("/joinsquarebtncomponent", (req, res) => {});
 
 squares.get("/postcomponent", (req, res) => {});
 
-squares.get("/subnavcomponent", (req, res) => {});
+squares.get("/subnavcomponent", (req, res) => {
+	res.render("components/subNavigation")
+});
 
 export default squares;
