5845b3fae4c5629ff3269699e022ddc1391321dd
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-04-25 12:18:19 -0700

    Finish dynamic item listing

--- routes/items.ts
+++ routes/items.ts
@@ -2,8 +2,21 @@ import express from "express";
 
 const items = express.Router();
 
+const item = {
+	id: 1,
+	name: "Wall Decor for Living Room",
+	description:
+		"2 modern minimalistic paintings. I don't like the style and would like to trade it for something more flashy.",
+	user: {
+		f_name: "John",
+		l_name: "Doe",
+		rating: 4.5,
+	},
+	likes: 20,
+};
+
 items.get("/all", (req, res) => {
-	res.render("pages/itemList");
+	res.render("pages/itemList", { items: item });
 });
 
 items.get("/itemcard", (req, res) => res.render("components/itemCard"));
