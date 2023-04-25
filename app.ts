6bebb8f75980bc422474845d1019040730b6bd77
scottchen98 <scott.chen1117@gmail.com>
2023-04-24 17:02:45 -0700

    update path

--- app.ts
+++ app.ts
@@ -6,12 +6,12 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("pages/index"));
 
 import squaresRouter from "./routes/squares";
 
 app.use("/squares", squaresRouter);
 
 app.listen(3000, () => {
-	console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
--- routes/squares.ts
+++ routes/squares.ts
@@ -3,13 +3,15 @@ import express from "express";
 const squares = express.Router();
 
 squares.get("/all", (req, res) => {
-	res.send("All the squares");
+  res.send("All the squares");
 });
-squares.get("/:id", (req, res) => {
-	res.send("Square for id: " + req.params.id);
-});
+// squares.get("/:id", (req, res) => {
+// 	res.send("Square for id: " + req.params.id);
+// });
 
-squares.get("/squarecomponent", (req, res) => {});
+squares.get("/squarecomponent", (req, res) => {
+  res.render("components/square");
+});
 
 squares.get("/squaretitlecomponent", (req, res) => {});
 
