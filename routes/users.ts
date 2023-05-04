766c469dd0b880335b424203baa344b0df33fbf9
Xiao Zhang <xxxiao971006@gmail.com>
2023-05-03 23:51:15 -0700

    Add likedItems in DB

--- routes/users.ts
+++ routes/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserById } from "../services/user";
+import { getUserById, getUserLikedItems } from "../services/user";
 import { Category, Item } from "@prisma/client";
 import { z } from "zod";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
@@ -41,6 +41,34 @@ const s3 = new S3Client({
 });
 
 const users = express.Router();
+
+
+users.get("/likes/:id", async (req, res) => {
+	const id = Number(req.params.id);
+	const userLikedItems = await getUserLikedItems(id);
+	if (userLikedItems) {
+		for (const item of userLikedItems) {
+			const getObjectParams = {
+				Bucket: bucketName,
+				Key: item.imgName,
+			};
+			const command = new GetObjectCommand(getObjectParams);
+			const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+			(
+				item as Item & {
+					imgUrl: string;
+				}
+			).imgUrl = url;
+		}
+		res.render("pages/likes", { userLikedItems, previousLink: req.headers.referer, });
+	}
+	else {
+		res.status(404).json({
+			message: "User not found",
+		});
+		return;
+	}
+})
 
 users.get("/:id", async (req, res) => {
 	const id = +req.params.id;
