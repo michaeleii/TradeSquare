2868b9ad603d4fb18ae11a839ed5091ac5e4be55
scottchen98 <scott.chen1117@gmail.com>
2023-05-03 23:47:16 -0700

    refactor code

--- app.ts
+++ app.ts
@@ -4,42 +4,7 @@ dotenv.config();
 const app = express();
 
 import { getAllItems } from "./services/item";
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { z } from "zod";
-
-const envVariables = z.object({
-	BUCKET_NAME: z.string(),
-	BUCKET_REGION: z.string(),
-	ACCESS_KEY: z.string(),
-	SECRET_ACCESS_KEY: z.string(),
-});
-
-envVariables.parse(process.env);
-
-declare global {
-	namespace NodeJS {
-		interface ProcessEnv extends z.infer<typeof envVariables> {
-			BUCKET_NAME: string;
-			BUCKET_REGION: string;
-			ACCESS_KEY: string;
-			SECRET_ACCESS_KEY: string;
-		}
-	}
-}
-
-const bucketName = process.env.BUCKET_NAME;
-const bucketRegion = process.env.BUCKET_REGION;
-const accessKey = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-
-const s3 = new S3Client({
-	credentials: {
-		accessKeyId: accessKey,
-		secretAccessKey: secretAccessKey,
-	},
-	region: bucketRegion,
-});
+import { getObjectSignedUrl } from "./s3";
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -49,33 +14,28 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-	try {
-		const items = await getAllItems();
-		if (!items) {
-			res.status(404).json({
-				message: "Items not found",
-			});
-			return;
-		}
+  try {
+    const items = await getAllItems();
+    if (!items) {
+      res.status(404).json({
+        message: "Items not found",
+      });
+      return;
+    }
 
-		for (const item of items) {
-			const getObjectParams = {
-				Bucket: bucketName,
-				Key: item.imgName,
-			};
-			const command = new GetObjectCommand(getObjectParams);
-			const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-			(
-				item as Item & {
-					user: User;
-					imgUrl: string;
-				}
-			).imgUrl = url;
-		}
-		res.render("pages/index", { items });
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    for (const item of items) {
+      const url = await getObjectSignedUrl(item.imgName);
+      (
+        item as Item & {
+          user: User;
+          imgUrl: string;
+        }
+      ).imgUrl = url;
+    }
+    res.render("pages/index", { items });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 import squaresRouter from "./routes/squares";
@@ -96,5 +56,5 @@ app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
