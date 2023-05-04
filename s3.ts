f054fc7893e1ed6b3c69f0ca9f14dfe0196a354e
scottchen98 <scott.chen1117@gmail.com>
2023-05-03 23:40:39 -0700

    refactor code

--- s3.ts
+++ s3.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import {
   S3Client,
   PutObjectCommand,
@@ -5,25 +7,46 @@ import {
   GetObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { z } from "zod";
 
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-const s3Client = new S3Client({
-  region,
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  },
+const envVariables = z.object({
+  BUCKET_NAME: z.string(),
+  BUCKET_REGION: z.string(),
+  ACCESS_KEY: z.string(),
+  SECRET_ACCESS_KEY: z.string(),
 });
 
-export function uploadFile(fileBuffer, fileName, mimetype) {
+envVariables.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {
+      BUCKET_NAME: string;
+      BUCKET_REGION: string;
+      ACCESS_KEY: string;
+      SECRET_ACCESS_KEY: string;
+    }
+  }
+}
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion,
+});
+
+export async function uploadFile(
+  fileBuffer: Buffer,
+  fileName: string,
+  mimetype: string
+) {
   const uploadParams = {
     Bucket: bucketName,
     Body: fileBuffer,
@@ -31,19 +54,19 @@ export function uploadFile(fileBuffer, fileName, mimetype) {
     ContentType: mimetype,
   };
 
-  return s3Client.send(new PutObjectCommand(uploadParams));
+  return await s3.send(new PutObjectCommand(uploadParams));
 }
 
-export function deleteFile(fileName) {
+export async function deleteFile(fileName: string) {
   const deleteParams = {
     Bucket: bucketName,
     Key: fileName,
   };
 
-  return s3Client.send(new DeleteObjectCommand(deleteParams));
+  return await s3.send(new DeleteObjectCommand(deleteParams));
 }
 
-export async function getObjectSignedUrl(key) {
+export async function getObjectSignedUrl(key: string) {
   const params = {
     Bucket: bucketName,
     Key: key,
@@ -51,8 +74,8 @@ export async function getObjectSignedUrl(key) {
 
   // https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
   const command = new GetObjectCommand(params);
-  const seconds = 60;
-  const url = await getSignedUrl(s3Client, command, { expiresIn: seconds });
+  const seconds = 3600;
+  const url = await getSignedUrl(s3, command, { expiresIn: seconds });
 
   return url;
 }
