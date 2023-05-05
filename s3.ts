2cbb0a962ee93897b9f9d317524b5cb4fac5be7e
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-05-05 13:42:12 -0700

    Import sharp

--- s3.ts
+++ s3.ts
@@ -6,6 +6,7 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { z } from "zod";
+import sharp from "sharp";
 
 const envVariables = z.object({
 	BUCKET_NAME: z.string(),
