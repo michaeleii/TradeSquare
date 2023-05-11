9488cff715b1e38b83a98df07146796f48567edc
Xiao Zhang <xxxiao971006@gmail.com>
2023-05-11 15:30:09 -0700

    pull

--- routes/test.ts
+++ routes/test.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import { getUserByAuth0Id } from "../services/user";
 
 const test = express.Router();
 
 // Do test.get(/<component name>/) for each component you want to test
+test.get("/mailbox", async (req, res) => {
+    const user = req.oidc.user ? await getUserByAuth0Id(req.oidc.user.sub) : null;    
+    if(user) {
+        res.render("pages/mailbox", { user });
+    }
+    else {
+        res.status(404).send("Users not found");
+        return;
+    }
+});
 
 export default test;
