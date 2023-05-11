fa6036431afff3ac8812f0a1199e05e1c1452da0
jerry <jerryfan71@gmail.com>
2023-05-11 15:26:47 -0700

    finish message page

--- routes/test.ts
+++ routes/test.ts
@@ -4,4 +4,8 @@ const test = express.Router();
 
 // Do test.get(/<component name>/) for each component you want to test
 
+test.get('/message', (req, res) => {
+    res.render('pages/myMessage');
+});
+
 export default test;
