a33dea9f91f2d2fb962896e92bf0a3af5e3c8049
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-05-11 12:55:58 -0700

    Finish styling userCredentialPage

--- routes/test.ts
+++ routes/test.ts
@@ -4,4 +4,8 @@ const test = express.Router();
 
 // Do test.get(/<component name>/) for each component you want to test
 
+test.get("/credentials", (req, res) =>
+  res.render("pages/credentials", { state: null })
+);
+
 export default test;
