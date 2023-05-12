7ff09bd25430359f6a538638b22f69ee58182c78
jerry <jerryfan71@gmail.com>
2023-05-11 20:10:51 -0700

    implemented auth0

--- routes/test.ts
+++ routes/test.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import { getUserByAuth0Id } from "../services/user";
 
 const test = express.Router();
 
 // Do test.get(/<component name>/) for each component you want to test
 
-test.get('/message', (req, res) => {
-    res.render('pages/myMessage');
+test.get('/message', async (req, res) => {
+    const user = req.oidc.user ? await getUserByAuth0Id(req.oidc.user.sub): null;
+    if (!user) {
+        res.status(404).send("User not found");
+    } else {
+        res.render('pages/myMessage');
+    }
 });
 
 export default test;
