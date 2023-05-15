b8067c9cc2b90ab7341e229c43be50b70371670b
jerry <jerryfan71@gmail.com>
2023-05-15 15:57:10 -0700

    add pre-profile page

--- routes/test.ts
+++ routes/test.ts
@@ -5,18 +5,21 @@ const test = express.Router();
 
 // Do test.get(/<component name>/) for each component you want to test
 
-test.get('/message', async (req, res) => {
-    const user = req.oidc.user ? await getUserByAuth0Id(req.oidc.user.sub): null;
-    if (!user) {
-        res.status(404).send("User not found");
-    } else {
-        res.render('pages/myMessage');
-    }
+test.get("/message", async (req, res) => {
+  const user = req.oidc.user ? await getUserByAuth0Id(req.oidc.user.sub) : null;
+  if (!user) {
+    res.status(404).send("User not found");
+  } else {
+    res.render("pages/myMessage");
+  }
 });
 
+test.get("/featureMessagePage", (req, res) => {
+  res.render("pages/featurePageMessage");
+});
 
-test.get('/featureMessagePage', (req, res) => {
-    res.render("pages/featurePageMessage");
-})
+test.get("/preFeatureProfile", (req, res) => {
+  res.render("pages/preFeatureProfilePage");
+});
 
 export default test;
