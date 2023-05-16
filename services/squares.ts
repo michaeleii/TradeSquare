62e7c49dfea586c8d50cc81e5e810c56cd83cccb
scottchen98 <scott.chen1117@gmail.com>
2023-05-16 11:15:12 -0700

    add sorted squares to carousel

--- services/squares.ts
+++ services/squares.ts
@@ -1,34 +1,45 @@
 import prisma from "../client";
 
 async function getAllSquares() {
-	try {
-		const allSquares = await prisma.square.findMany();
-		return allSquares;
-	} catch (err) {
-		console.log(err);
-		return null;
-	}
+  try {
+    const allSquares = await prisma.square.findMany();
+    return allSquares;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+async function getAllSortedSquares() {
+  try {
+    const squares = await prisma.square.findMany({
+      orderBy: { users: { _count: "desc" } },
+    });
+    return squares;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getSquareById(id: number) {
-	try {
-		const square = await prisma.square.findUnique({
-			where: {
-				id: id,
-			},
-			include: {
-				posts: {
-					include: {
-						user: true,
-					},
-				},
-			},
-		});
-		return square;
-	} catch (err) {
-		console.log(err);
-		return null;
-	}
+  try {
+    const square = await prisma.square.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        posts: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+    return square;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
-export { getAllSquares, getSquareById };
+export { getAllSquares, getAllSortedSquares, getSquareById };
