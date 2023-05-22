a1da38949b96ef96f36e90577c6ef337f9db609b
Michael Lei <michaelleie0ca@gmail.com>
2023-05-22 00:38:20 -0700

    Add touch for like icon

--- public/src/like.js
+++ public/src/like.js
@@ -1,19 +1,37 @@
 const likeIcon = document.querySelector(".like-icon");
 
 likeIcon.addEventListener("click", async () => {
-	if (likeIcon.classList.contains("liked")) {
-		likeIcon.classList.remove("liked");
-	} else {
-		likeIcon.classList.add("liked");
-	}
-	const options = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			itemId: likeIcon.dataset.itemId,
-		}),
-	};
-	await fetch(`/api/item/like/`, options);
+  if (likeIcon.classList.contains("liked")) {
+    likeIcon.classList.remove("liked");
+  } else {
+    likeIcon.classList.add("liked");
+  }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      itemId: likeIcon.dataset.itemId,
+    }),
+  };
+  await fetch(`/api/item/like/`, options);
+});
+
+likeIcon.addEventListener("touchend", async () => {
+  if (likeIcon.classList.contains("liked")) {
+    likeIcon.classList.remove("liked");
+  } else {
+    likeIcon.classList.add("liked");
+  }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      itemId: likeIcon.dataset.itemId,
+    }),
+  };
+  await fetch(`/api/item/like/`, options);
 });
