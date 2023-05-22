b9e486cb192523dfba4ffbde83894aee70680e72
Michael Lei <michaelleie0ca@gmail.com>
2023-05-21 20:17:18 -0700

    Add css variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       colors: {
         bkg: "rgb(var(--color-bkg) / <alpha-value>)",
         content: "rgb(var(--color-content) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary-bkg) / <alpha-value>)",
+        heading: "rgb(var(--color-content-heading) / <alpha-value>)",
+        stroke: "rgb(var(--color-stroke) / <alpha-value>)",
         primary: {
           DEFAULT: "#506DF6",
           dark: "#3B4BDB",
