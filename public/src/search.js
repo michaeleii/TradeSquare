46d20b4e8a5eff1da63f323003668021af8726ed
scottchen98 <scott.chen1117@gmail.com>
2023-05-22 00:05:59 -0700

    fix search result msg

--- public/src/search.js
+++ public/src/search.js
@@ -1,15 +1,21 @@
 const form = document.getElementById("searchBar");
 const items = document.querySelectorAll(".item-card");
+const headingTitle = document.querySelector("#headingTitle");
+const noItemsMsg = document.querySelector("#noItemsMsg");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
   const query = formData.get("search");
+  headingTitle.innerText = `Search Results for: ${query}`;
+  let count = 0;
   Array.from(items).forEach((item) => {
     const itemName = item.querySelector(".item-name").innerText;
     if (itemName.toLowerCase().includes(query.toLowerCase())) {
       item.style.display = "block";
+      count++;
     } else {
       item.style.display = "none";
     }
   });
+  noItemsMsg.style.display = !count ? "block" : "none";
 });
